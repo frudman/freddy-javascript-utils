cc81ec9ff0687f4b175478545ee95f15fc49cbea
Freddy Rudman <frederic.rudman@simplytel.com>
2019-01-21 14:17:24 -0500

    working code

--- utils.js
+++ utils.js
@@ -52,3 +52,5 @@ export function genCombinations_with_callback(options, cb) {
         propAlts.forEach(v => genCombos(otherProps, subAlts => cb(Object.assign({ [thisProp]: v }, subAlts))))
     }
 }
+
+export const pi = 3.1423;
